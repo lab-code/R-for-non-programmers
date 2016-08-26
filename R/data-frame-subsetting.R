65caf2e36ec10d00e5a608e7388387058ac30236
hejtmy <hejtmy@gmail.com>
2016-08-26 14:08:39 +0200

    placeholder for data frame subsetting

---
+++ R/data-frame-subsetting.R
@@ -0,0 +1,21 @@
+# read the table using read.csv function
+
+# look at the table
+
+# you can do simple descriptive statistics
+
+# extract a vector of year published
+
+# find vector of indices of all movies released after 2000
+
+# vector of indices of all movies released between 2000 and 1980
+
+# extract vecotr of movie types
+
+# find logical vector of all TV series in the data frame
+
+# find logical vector of all things that arent a Feature movie
+
+# Let's combine two things
+
+# Find logical vector of all Tv series released after 2000
