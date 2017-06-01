5e429efa75ca67d05ae4bd2aafa0ebd14af75845
hejtmy <hejtmy@gmail.com>
2017-06-01 11:02:59 +0200

    Renames things so it's clear which comes first

--- R/Session1/1-r-in-action.R
+++ R/Session1/1-r-in-action.R
@@ -19,16 +19,23 @@ df_movies[order(-df_movies$Num.Votes), ][1, ]
 
 
 # What are my favorite movies?
+head(df_movies[order(-df_movies$You.rated), ], 10)
 
+# WHat movies did I not agree with the rest of the world on?
+View(head(df_movies[order(df_movies$You.rated - df_movies$IMDb.Rating),], 10))
 
 ## Descrptive statistics
 #what is a mean rating I gave?
 
 # Graph a histogram of my ratings
-
-
+hist(df_movies$You.rated)
+ggplot(df_movies, aes(You.rated)) + geom_histogram(bins = 10)
 # Graph a histogram for my ratings only for movies before 2000.
+ggplot(df_movies[df_movies$Year > 2000, ], aes(You.rated)) + geom_histogram(bins = 10)
 
+df_movies$above2000 <- df_movies$Year > 2000
+
+ggplot(df_movies, aes(You.rated, fill = above2000)) + geom_histogram(bins = 10)
 
 # Let's get complex!
 # What 
